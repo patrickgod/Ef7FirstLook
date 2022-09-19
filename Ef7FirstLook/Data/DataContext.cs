0e58fc494a5dbf72e26a79f76e5fbca89ebf4dd6
Patrick <patrick@godknows.de>
2022-09-19 22:05:34 +0200

    Add project files.

---
+++ Ef7FirstLook/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ef7FirstLook.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=dotnet7testdb;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+
+        public DbSet<WeatherForecast> Forecasts { get; set; }
+    }
+}
---
+++ Ef7FirstLook/WeatherForecast.cs
@@ -0,0 +1,15 @@
+namespace Ef7FirstLook
+{
+    public class WeatherForecast
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
